19d80b156bbbd680770f0d8672381cc8af8dd0e2
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2024-09-21 19:51:13 +0300

    finshes learning work with templates. so let's go to play with database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+  {
+    'author': 'CoreyMS',
+    'title': 'blog post 1',
+    'content': 'first post content',
+    'date_posted': 'Augest, 27 2018'
+  },
+  {
+    'author': 'Jane Doe',
+    'title': 'blog post 2',
+    'content': 'first post content',
+    'date_posted': 'Augest, 27 2018'
+  }
+]
 
 def home(request):
-  return HttpResponse('<h1>Blog Home</h1>')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html',context)
 
 
 def about(request):
-  return HttpResponse('<h1>Blog about</h1>')
+  return render(request, 'blog/about.html', {'title': 'About'})
